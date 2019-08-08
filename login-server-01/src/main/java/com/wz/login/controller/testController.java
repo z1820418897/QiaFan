c99d825c7b36f0cad83fb700f5052abdb7374685
Wq <1821478380@qq.com>
2019-08-08 16:02:39 +0800

    配置
    yml配置修改

--- login-server-01/src/main/java/com/wz/login/controller/testController.java
+++ login-server-01/src/main/java/com/wz/login/controller/testController.java
@@ -3,6 +3,8 @@ package com.wz.login.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 public class testController {
 
@@ -10,5 +12,14 @@ public class testController {
     public String test(){
 
         return "你好世界";
+    }
+
+    @RequestMapping("/*")
+    public String IpAddr(HttpServletRequest request){
+        String ip = "服务地址 : ";
+        String remoteAddr = request.getServerName();
+        int serverPort = request.getServerPort();
+        ip += remoteAddr+" --端口:"+serverPort;
+        return ip;
     }
 }
